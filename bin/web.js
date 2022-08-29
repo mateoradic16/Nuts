d0615a7fc152f53988c0cd2fb847f287d1c76fc7
Mateo Radic <mateoradic16@gmail.com>
2022-08-29 23:21:21 +0200

    Added DotEnv

--- bin/web.js
+++ bin/web.js
@@ -1,18 +1,20 @@
-var express = require('express');
-var uuid = require('uuid');
-var basicAuth = require('basic-auth');
-var Analytics = require('analytics-node');
-var nuts = require('../');
+var express = require("express");
+var uuid = require("uuid");
+var basicAuth = require("basic-auth");
+var Analytics = require("analytics-node");
+var nuts = require("../");
+var dotenv = require("dotenv");
+dotenv.config();
 
 var app = express();
 
-var apiAuth =  {
+var apiAuth = {
     username: process.env.API_USERNAME,
-    password: process.env.API_PASSWORD
+    password: process.env.API_PASSWORD,
 };
 
 var analytics = undefined;
-var downloadEvent = process.env.ANALYTICS_EVENT_DOWNLOAD || 'download';
+var downloadEvent = process.env.ANALYTICS_EVENT_DOWNLOAD || "download";
 if (process.env.ANALYTICS_TOKEN) {
     analytics = new Analytics(process.env.ANALYTICS_TOKEN);
 }
@@ -26,37 +28,55 @@ var myNuts = nuts.Nuts({
     timeout: process.env.VERSIONS_TIMEOUT,
     cache: process.env.VERSIONS_CACHE,
     refreshSecret: process.env.GITHUB_SECRET,
-    proxyAssets: !Boolean(process.env.DONT_PROXY_ASSETS)
+    proxyAssets: !Boolean(process.env.DONT_PROXY_ASSETS),
 });
 
 // Control access to API
-myNuts.before('api', function(access, next) {
+myNuts.before("api", function(access, next) {
     if (!apiAuth.username) return next();
 
     function unauthorized() {
-        next(new Error('Invalid username/password for API'));
-    };
+        next(new Error("Invalid username/password for API"));
+    }
 
     var user = basicAuth(access.req);
     if (!user || !user.name || !user.pass) {
         return unauthorized();
-    };
+    }
 
     if (user.name === apiAuth.username && user.pass === apiAuth.password) {
         return next();
     } else {
         return unauthorized();
-    };
+    }
 });
 
 // Log download
-myNuts.before('download', function(download, next) {
-    console.log('download', download.platform.filename, "for version", download.version.tag, "on channel", download.version.channel, "for", download.platform.type);
+myNuts.before("download", function(download, next) {
+    console.log(
+        "download",
+        download.platform.filename,
+        "for version",
+        download.version.tag,
+        "on channel",
+        download.version.channel,
+        "for",
+        download.platform.type
+    );
 
     next();
 });
-myNuts.after('download', function(download, next) {
-    console.log('downloaded', download.platform.filename, "for version", download.version.tag, "on channel", download.version.channel, "for", download.platform.type);
+myNuts.after("download", function(download, next) {
+    console.log(
+        "downloaded",
+        download.platform.filename,
+        "for version",
+        download.version.tag,
+        "on channel",
+        download.version.channel,
+        "for",
+        download.platform.type
+    );
 
     // Track on segment if enabled
     if (analytics) {
@@ -64,14 +84,14 @@ myNuts.after('download', function(download, next) {
 
         analytics.track({
             event: downloadEvent,
-            anonymousId: userId? null : uuid.v4(),
+            anonymousId: userId ? null : uuid.v4(),
             userId: userId,
             properties: {
                 version: download.version.tag,
                 channel: download.version.channel,
                 platform: download.platform.type,
-                os: nuts.platforms.toType(download.platform.type)
-            }
+                os: nuts.platforms.toType(download.platform.type),
+            },
         });
     }
 
@@ -81,10 +101,9 @@ myNuts.after('download', function(download, next) {
 if (process.env.TRUST_PROXY) {
     try {
         var trustProxyObject = JSON.parse(process.env.TRUST_PROXY);
-        app.set('trust proxy', trustProxyObject);
-    }
-    catch (e) {
-        app.set('trust proxy', process.env.TRUST_PROXY);
+        app.set("trust proxy", trustProxyObject);
+    } catch (e) {
+        app.set("trust proxy", process.env.TRUST_PROXY);
     }
 }
 
@@ -102,32 +121,36 @@ app.use(function(err, req, res, next) {
 
     // Return error
     res.format({
-        'text/plain': function(){
+        "text/plain": function() {
             res.status(code).send(msg);
         },
-        'text/html': function () {
+        "text/html": function() {
             res.status(code).send(msg);
         },
-        'application/json': function (){
+        "application/json": function() {
             res.status(code).send({
-                'error': msg,
-                'code': code
+                error: msg,
+                code: code,
             });
-        }
+        },
     });
 });
 
-myNuts.init()
+myNuts
+    .init()
 
 // Start the HTTP server
-.then(function() {
-    var server = app.listen(process.env.PORT || 5000, function () {
-        var host = server.address().address;
-        var port = server.address().port;
+.then(
+    function() {
+        var server = app.listen(process.env.PORT || 5000, function() {
+            var host = server.address().address;
+            var port = server.address().port;
 
-        console.log('Listening at http://%s:%s', host, port);
-    });
-}, function(err) {
-    console.log(err.stack || err);
-    process.exit(1);
-});
+            console.log("Listening at http://%s:%s", host, port);
+        });
+    },
+    function(err) {
+        console.log(err.stack || err);
+        process.exit(1);
+    }
+);
